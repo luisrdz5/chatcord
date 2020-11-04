808b9713315812cc7329855668a7cf9964f4f64f
Luis Antonio Rodriguez Garcia <luis@luisrodriguezgarcia.com>
2020-11-04 14:43:39 -0600

    fixed some bugs

--- realtimeAPI/libs/celery.js
+++ realtimeAPI/libs/celery.js
@@ -1,6 +1,6 @@
 const celery = require('celery-node');
 
-const brokerUrl = 'redis://redis:6379/0';
+const brokerUrl = 'redis://ip172-18-0-123-buhfsr1qckh0008ru550-3000.direct.labs.play-with-docker.com:6379/0';
 const backend = 'redis://';
 
 const client = celery.createClient(brokerUrl, backend);
